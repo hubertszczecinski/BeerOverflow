b682aa6918dbddbe173f5ec2a4cb717b2b4704c1
Jan Pawlowski <jan_pawlowski@outlook.com>
2025-11-14 15:49:16 +0100

    home page and components prototypes

---
+++ app/frontend/src/components/ActionBox.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="border-gray-400 border-gray-200">
+  <img src="../assets/cmz_logo.png" alt="logo" ></img>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/frontend/src/components/Actionboxes.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ActionBox from "@/components/ActionBox.vue";
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
